9cf321e25a89d384324c6fa32be17c9227b2c630
jstD <dung.truong2000@hcmut.edu.vn>
2021-03-27 14:54:53 +0700

    merge sort update

---
+++ heapSort.py
@@ -0,0 +1,29 @@
+def mergeSort(lst):
+    if len(lst) > 1:
+        mid = len(lst)//2
+        left = lst[:mid]
+        right = lst[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1 
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+
+lst = [3, 3, 4, 6, 5, 7, 9, 11, 3, 32, 13, 8]
+mergeSort(lst)
+print(lst)
